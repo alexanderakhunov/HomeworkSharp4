c3e47f8806b4e30a8de46d6b58fe8273b35e1723
AlexanderAkh <sancheek240@mail.ru>
2023-01-31 17:41:25 +0300

    Add task 3

---
+++ Task3/Program.cs
@@ -0,0 +1,8 @@
+﻿
+Random rand = new Random();
+int [] array = new int[8];
+for(int i =0; i<array.Length; i++)
+{
+    array[i] = rand.Next(0,100);
+}
+System.Console.WriteLine(string.Join(", ", array));
