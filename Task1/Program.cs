3db4286cc435b1c33a4360549560d54969fe5510
AlexanderAkh <sancheek240@mail.ru>
2023-01-31 17:12:24 +0300

    Add task1

---
+++ Task1/Program.cs
@@ -0,0 +1,32 @@
+﻿int inputNumber(string str)
+{
+    int number;
+    string text;
+    while (true)
+    {
+        System.Console.Write(str);
+        text = Console.ReadLine();
+
+        if (int.TryParse(text, out number))
+        {
+            break;
+        }
+        System.Console.WriteLine("Неверное число");
+    }
+    return number;
+}
+
+// 
+int num = inputNumber("Введите число ");
+int SR = inputNumber("Введите степень ");
+int SQR(int number)
+{
+int res = 1;
+for (int i = 1; i<=SR; i++)
+{
+res *=num;
+}
+return res;
+}
+int result = SQR(SR);
+System.Console.WriteLine(result);
